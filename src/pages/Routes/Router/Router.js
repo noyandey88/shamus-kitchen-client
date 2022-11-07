941aae66b31918ebf548b82620e738b1d65a65e1
codernoyan <coder.noyan@gmail.com>
2022-11-08 00:15:55 +0600

    assignment project initialized

---
+++ src/pages/Routes/Router/Router.js
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../../ErrorPage/ErrorPage";
+import Home from "../../Home/Home/Home";
+import Main from "../../Layouts/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+      {
+        path: '/home',
+        element: <Home></Home>
+      }
+    ]
+  }
+])
