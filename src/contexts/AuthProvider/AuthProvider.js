2e950b5a29cfa290cd7d4a2d86a2533ca954ca39
codernoyan <coder.noyan@gmail.com>
2022-11-09 02:39:38 +0600

    created and added auth context data

---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,49 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import app from '../../firebase/firebase.config';
+
+const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOutUser = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    })
+    return () => unsubscribe();
+  }, [])
+
+  const authInfo = {
+    createUser,
+    loginUser,
+    logOutUser,
+    user,
+    loading
+  }
+  return (
+    <AuthContext.Provider value={authInfo}>
+      
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
