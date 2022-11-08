563853aff37700e660848387f791c227a418eee1
codernoyan <coder.noyan@gmail.com>
2022-11-08 12:03:14 +0600

    footer created

--- src/pages/Layouts/Main.js
+++ src/pages/Layouts/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Footer from '../Shared/Footer/Footer';
+import FooterPage from '../Shared/FooterPage/FooterPage';
 import Header from '../Shared/Header/Header';
 
 const Main = () => {
@@ -8,7 +8,7 @@ const Main = () => {
     <div>
       <Header></Header>
       <Outlet></Outlet>
-      <Footer></Footer>
+      <FooterPage></FooterPage>
     </div>
   );
 };
