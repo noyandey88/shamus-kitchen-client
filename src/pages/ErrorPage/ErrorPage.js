15cba0f7de96bc6fb6f4e91934b967cf28f020e9
codernoyan <coder.noyan@gmail.com>
2022-11-08 12:07:22 +0600

    added meaningful error page

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
+  const error = useRouteError();
   return (
     <section className="flex items-center h-full p-16">
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
@@ -9,7 +10,8 @@ const ErrorPage = () => {
           <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
             <span className="sr-only">Error</span>404
           </h2>
-          <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+          {/* <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p> */}
+          <p className="text-2xl font-semibold md:text-3xl"><em>{error.statusText || error.message}</em></p>
           <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
           <Link rel="noopener noreferrer" to="/" className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
         </div>
