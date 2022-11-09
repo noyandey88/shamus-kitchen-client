6d04ee3ba55748cf798d037e5db113bdf7aa6463
codernoyan <coder.noyan@gmail.com>
2022-11-09 18:36:11 +0600

    added post review input field

--- src/pages/ServiceDetails/ServiceDetails.js
+++ src/pages/ServiceDetails/ServiceDetails.js
@@ -1,3 +1,4 @@
+import { Button, Label, Textarea, TextInput } from 'flowbite-react';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
@@ -11,7 +12,7 @@ const ServiceDetails = () => {
       <div className="text-center mt-4">
         <h1 className="text-4xl font-semibold">{name} Details</h1>
       </div>
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-8">
         <div className="flex flex-col max-w-screen-lg overflow-hidden bg-white border rounded shadow-sm lg:flex-row sm:mx-auto">
           <div className="relative lg:w-1/2">
             <img src={imageUrl}
@@ -32,13 +33,45 @@ const ServiceDetails = () => {
             <p className="mb-5 text-gray-800">
               {description}
             </p>
-            <h4 className="text 2xl font-semibold">Price: {price}$</h4>
-            <h4 className="text 2xl font-semibold">Average Ratings: {ratings}</h4>
+            <h4 className="text-2xl font-bold">Price: <span className="text-orange-500">{price}$</span></h4>
+            <h4 className="text-2xl font-bold">Average Ratings: <span className="text-orange-500">{ratings}</span></h4>
           </div>
         </div>
       </div>
       {/* details end */}
-
+      {/* review section start */}
+      <div className="text-center mt-4">
+        <h1 className="text-4xl font-semibold">Customer <span className="text-orange-500 font-bold">Reviews</span></h1>
+      </div>
+      <hr className="mt-4" />
+      <div className="w-3/4 mx-auto">
+        <h2 className="text-2xl font-semibold">Post a <span className="text-orange-500 font-bold">Review:</span></h2>
+        <form className="flex flex-col gap-4 my-3">
+          <div>
+            <TextInput
+              type="text"
+              placeholder="Name"
+            />
+          </div>
+          <div>
+            <TextInput
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div>
+            <Textarea
+              id="comment"
+              placeholder="Leave a review text..."
+              required={true}
+              rows={4}
+            />
+          </div>
+          <button className="w-full py-2 bg-orange-500 text-white font-semibold rounded-md" type="submit">
+            Post
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
